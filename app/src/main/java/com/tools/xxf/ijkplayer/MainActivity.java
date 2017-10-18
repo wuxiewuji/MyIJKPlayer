f92a39d5ea5a2a6b11926f63975eb50aaf609011
git_xxf <git_xxf@local.com>
2017-10-18 10:44:29 +0800

    添加播放基本设置

--- app/src/main/java/com/tools/xxf/ijkplayer/MainActivity.java
+++ app/src/main/java/com/tools/xxf/ijkplayer/MainActivity.java
@@ -1,13 +1,184 @@
 package com.tools.xxf.ijkplayer;
 
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.Surface;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.tools.xxf.ijkplayer.common.PlayerManager;
+import com.tools.xxf.ijkplayer.widget.media.AndroidMediaController;
+import com.tools.xxf.ijkplayer.widget.media.IjkVideoView;
+
+public class MainActivity extends AppCompatActivity implements PlayerManager.PlayerStateListener {
+
+    private IjkVideoView videoView;
+    private PlayerManager player;
+    private FrameLayout ffView;
+    private LinearLayout view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ffView = (FrameLayout) findViewById(R.id.ffplayer);
+        videoView = (IjkVideoView) findViewById(R.id.video_view);
+        view = (LinearLayout) findViewById(R.id.view);
+
+        player = new PlayerManager(this);
+        player.setScaleType(PlayerManager.SCALETYPE_FILLPARENT);
+        player.playInFullScreen(false);
+        player.setPlayerStateListener(this);
+        player.play("http://221.4.223.101:8000/media/49_720p.flv");
+
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.back://返回控件
+                break;
+            case R.id.full://全屏设置
+                onExpendScreen();
+                break;
+            case R.id.player://暂停，播放
+                if (player.isPlaying()) {
+                    player.onPause();
+                } else {
+                    player.start();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private int getScreenOrientation() {
+        int rotation = getWindowManager().getDefaultDisplay().getRotation();
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+        int orientation;
+        // if the device's natural orientation is portrait:
+        if ((rotation == Surface.ROTATION_0 || rotation == Surface.ROTATION_180) && height >
+                width ||
+                (rotation == Surface.ROTATION_90 || rotation == Surface.ROTATION_270) && width >
+                        height) {
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                    break;
+                case Surface.ROTATION_90:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                    break;
+                case Surface.ROTATION_180:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+                    break;
+                case Surface.ROTATION_270:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+                    break;
+                default:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                    break;
+            }
+        }
+        // if the device's natural orientation is landscape or if the device
+        // is square:
+        else {
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                    break;
+                case Surface.ROTATION_90:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT;
+                    break;
+                case Surface.ROTATION_180:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE;
+                    break;
+                case Surface.ROTATION_270:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+                    break;
+                default:
+                    orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE;
+                    break;
+            }
+        }
+        return orientation;
+    }
+
+    /**
+     * 横竖屏判断
+     */
+    private boolean isPortrait() {
+        int orientation = getScreenOrientation();
+        boolean portrait = orientation == ActivityInfo.SCREEN_ORIENTATION_PORTRAIT ||
+                orientation == ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT;
+        return portrait;
+    }
+
+    // small full screen
+    private int playerNormalHeight = 0;
+
+    /**
+     * 横竖屏切换，实现全屏
+     */
+    private void onExpendScreen() {
+        if (isPortrait()) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) ffView.getLayoutParams();
+            if (0 == playerNormalHeight)
+                playerNormalHeight = params.height;
+            params.height =LinearLayout.LayoutParams.MATCH_PARENT;
+            ffView.requestLayout();
+
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) ffView.getLayoutParams();
+            params.height = playerNormalHeight;
+            ffView.requestLayout();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (player.gestureDetector.onTouchEvent(event))
+            return true;
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+
+    @Override
+    public void onError() {
+
+    }
+
+    @Override
+    public void onLoading() {
+
+    }
+
+    @Override
+    public void onPlay() {
+
     }
 }
---
+++ app/src/main/java/com/tools/xxf/ijkplayer/widget/media/Permissions.java
@@ -0,0 +1,108 @@
+/*
+ * *************************************************************************
+ *  Permissions.java
+ * **************************************************************************
+ *  Copyright © 2015 VLC authors and VideoLAN
+ *  Author: Geoffrey Métais
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *  ***************************************************************************
+ */
+
+package com.tools.xxf.ijkplayer.widget.media;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
+
+import com.tools.xxf.ijkplayer.R;
+
+
+public class Permissions {
+
+    public static final int PERMISSION_SYSTEM_RINGTONE = 42;
+    public static final int PERMISSION_SYSTEM_BRIGHTNESS = 43;
+    /*
+     * Marshmallow permission system management
+     */
+    public static final boolean isNougatOrLater = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
+    public static final boolean isMarshMallowOrLater = isNougatOrLater || Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+
+    @TargetApi(Build.VERSION_CODES.M)
+    public static boolean canWriteSettings(Context context) {
+        return isMarshMallowOrLater || Settings.System.canWrite(context);
+    }
+
+
+    public static void checkWriteSettingsPermission(Activity activity, int mode) {
+        if (!canWriteSettings(activity)) {
+            showSettingsPermissionDialog(activity, mode);
+        }
+    }
+
+    private static Dialog sAlertDialog;
+
+    public static void showSettingsPermissionDialog(final Activity activity, int mode) {
+        if (activity.isFinishing() || (sAlertDialog != null && sAlertDialog.isShowing()))
+            return;
+        sAlertDialog = createSettingsDialogCompat(activity, mode);
+    }
+
+
+    private static Dialog createSettingsDialogCompat(final Activity activity, int mode) {
+        int titleId = 0, textId = 0;
+        String action = Settings.ACTION_MANAGE_WRITE_SETTINGS;
+        switch (mode) {
+            case PERMISSION_SYSTEM_RINGTONE:
+                titleId = R.string.allow_settings_access_ringtone_title;
+                textId = R.string.allow_settings_access_ringtone_description;
+                break;
+            case PERMISSION_SYSTEM_BRIGHTNESS:
+                titleId = R.string.allow_settings_access_brightness_title;
+                textId = R.string.allow_settings_access_brightness_description;
+                break;
+        }
+        final String finalAction = action;
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity)
+                .setTitle(activity.getString(titleId))
+                .setMessage(activity.getString(textId))
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setPositiveButton(activity.getString(R.string.permission_ask_again), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(activity);
+                        Intent i = new Intent(finalAction);
+                        i.setData(Uri.parse("package:" + activity.getPackageName()));
+                        try {
+                            activity.startActivity(i);
+                        } catch (Exception ex) {}
+                        SharedPreferences.Editor editor = settings.edit();
+                        editor.putBoolean("user_declined_settings_access", true);
+                        editor.apply();
+                    }
+                });
+        return dialogBuilder.show();
+    }
+
+}
